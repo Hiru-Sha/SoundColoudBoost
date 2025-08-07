8b5723be8baeb9eafb704e067936e4b4cd5f489d
Hirusha Rashmika <hirusharashmika3@gmail.com>
2025-08-07 13:18:23 +0530

    m

---
+++ middleware/upload.js
@@ -0,0 +1,17 @@
+const multer = require('multer');
+const path = require('path');
+
+// Set up storage engine
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, '../uploads'));
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+    
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
